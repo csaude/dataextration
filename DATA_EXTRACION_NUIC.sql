473913b8768d01fe3513f0c0214321c0024907a1
mshueib <shueib007@gmail.com>
2022-06-27 14:38:41 +0200

    Update Delete children age with nuic null

--- DATA_EXTRACION_NUIC.sql
+++ DATA_EXTRACION_NUIC.sql
@@ -154,8 +154,7 @@ UPDATE children,
 SET children.date_of_ART_initiation=inicio_real.data_inicio
 WHERE children.patient_id=inicio_real.patient_id;
 
-delete from children where children.age_enrollment<=13;
-
+delete from children where children.age_enrollment<=13 and children.nuic is null;
 
 /*Urban e Main*/
 update children set urban='N';
